1cdaadb18fab35cf1cddec88b6915b02a0c4cecc
MD ARIFUL HASAN <77568259+mdahtotul@users.noreply.github.com>
2024-08-10 22:13:17 +0600

    feat: queue working

--- app/core/envs.py
+++ app/core/envs.py
@@ -10,4 +10,7 @@ celery_broker_url = os.environ.get(
     "CELERY_BROKER_URL",
     "redis://redis:6379/0",
 )
-celery_result_backend = os.environ.get("CELERY_RESULT_BACKEND", "redis://redis:6379/0")
+celery_result_backend = os.environ.get(
+    "CELERY_RESULT_BACKEND",
+    "redis://redis:6379/0",
+)
