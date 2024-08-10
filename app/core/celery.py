54bba2893bc991bf21d69a9e9a227694f7fb23e8
MD ARIFUL HASAN <77568259+mdahtotul@users.noreply.github.com>
2024-08-10 21:39:37 +0600

    feat: queue adding

--- app/core/celery.py
+++ app/core/celery.py
@@ -8,10 +8,16 @@ app = Celery("django_celery")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
 # task routing
-app.conf.task_routes = {
-    # "app.tasks.*": {"queue": "celery"},
-    "playground.tasks.task1": {"queue": "queue1"},
-    "playground.tasks.task2": {"queue": "queue2"},
+# app.conf.task_routes = {
+#     # "app.tasks.*": {"queue": "celery"},
+#     "playground.tasks.task1": {"queue": "queue1"},
+#     "playground.tasks.task2": {"queue": "queue2"},
+# }
+
+app.conf.broker_transport_options = {
+    "priority_steps": list(range(10)),
+    "sep": ":",
+    "queue_order_strategy": "priority",
 }
 
 app.autodiscover_tasks()
