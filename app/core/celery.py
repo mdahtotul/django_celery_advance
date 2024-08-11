dacf5f7a25f91aeb71a0eb20f18e613e5328dc82
MD ARIFUL HASAN <77568259+mdahtotul@users.noreply.github.com>
2024-08-11 15:41:02 +0600

    feat: rabbitmq working

--- app/core/celery.py
+++ app/core/celery.py
@@ -1,12 +1,50 @@
 import os
-
+import time
 from celery import Celery
+from kombu import Queue, Exchange
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
 app = Celery("django_celery")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
+########### RabbitMQ ################
+app.conf.task_queues = [
+    Queue(
+        "tasks",
+        Exchange("tasks"),
+        routing_key="tasks",
+        queue_arguments={"x-max-priority": 10},
+    )
+]
+app.conf.task_acks_late = True
+app.conf.task_default_priority = 5
+app.conf.worker_prefetch_multiplier = 1
+app.conf.worker_concurrency = 1
+
+
+@app.task(queue="tasks")
+def t1():
+    time.sleep(3)
+    return
+
+
+@app.task(queue="tasks")
+def t2():
+    time.sleep(3)
+    return
+
+
+@app.task(queue="tasks")
+def t3():
+    time.sleep(3)
+    return
+
+
+########### RabbitMQ ################
+
+
+########### Redis ################
 # task routing
 # app.conf.task_routes = {
 #     # "app.tasks.*": {"queue": "celery"},
@@ -15,10 +53,10 @@ app.config_from_object("django.conf:settings", namespace="CELERY")
 # }
 
 
-app.conf.broker_transport_options = {
-    "priority_steps": list(range(10)),
-    "sep": ":",
-    "queue_order_strategy": "priority",
-}
-
+# app.conf.broker_transport_options = {
+#     "priority_steps": list(range(10)),
+#     "sep": ":",
+#     "queue_order_strategy": "priority",
+# }
+########### Redis ################
 app.autodiscover_tasks()
--- app/core/envs.py
+++ app/core/envs.py
@@ -8,7 +8,8 @@ allowed_hosts = os.environ.get("ALLOWED_HOSTS")
 # celery envs
 celery_broker_url = os.environ.get(
     "CELERY_BROKER_URL",
-    "redis://redis:6379/0",
+    "amqp://guest:guest@rabbitmq:5672/",
+    # "redis://redis:6379/0",
 )
 celery_result_backend = os.environ.get(
     "CELERY_RESULT_BACKEND",
